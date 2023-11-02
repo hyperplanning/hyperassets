82c361a4dbb95d2c6ae47a5237c18b4ee3420d9c
ErwinR <erwin@hyperplan.fr>
2023-11-02 15:06:45 +0100

    feat: init python project to pip dependency

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="hyperassets",
+    version="1.0.0",
+    packages=find_packages(),
+    install_requires=[
+        # List your dependencies here
+    ],
+)
